df8360830a0af7ffea4f869f20f3dcde91ac5277
PouyaBapiry <pouyabapiry83@gmail.com>
2026-06-10 12:51:55 +0330

    Main Template added to project

---
+++ Pharmacy.Web/ViewComponents/SiteViewComponent.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.ViewComponents
+{
+    #region Site Header
+
+    public class SiteHeaderViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("SiteHeader");
+        }
+    }
+
+    #endregion
+
+    #region Site Footer
+    public class SiteFooterViewComponent : ViewComponent
+    {
+        //private readonly ISiteSettingService _siteSettingService;
+
+        //public SiteFooterViewComponent(ISiteSettingService siteSettingService)
+        //{
+        //    _siteSettingService = siteSettingService;
+        //}
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            
+            return View("SiteFooter");
+        }
+    }
+    #endregion
+
+    #region Mega Menu
+
+    public class MegaMenuViewComponent : ViewComponent
+    {
+        //private readonly IProductService _productService;
+
+        //public MegaMenuViewComponent(IProductService productService)
+        //{
+        //    _productService = productService;
+        //}
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //var category = await _productService.GetAllActiveProductCategories();
+            //ViewBag.ProductCategories = await _productService.GetAllActiveProductCategories();
+            return View("MegaMenu");
+        }
+    }
+    #endregion
+
+    //#region Latest Arrivals
+
+    //public class LatestArrivalProductViewComponent : ViewComponent
+    //{
+    //    private readonly IProductService _productService;
+
+    //    public LatestArrivalProductViewComponent(IProductService productService)
+    //    {
+    //        _productService = productService;
+    //    }
+
+    //    public async Task<IViewComponentResult> InvokeAsync()
+    //    {
+    //        var latestArrival = await _productService.GetLatestArrivalProducts(15);
+    //        return View("LatestArrivalProduct", latestArrival);
+    //    }
+    //}
+
+    //#endregion
+
+    //#region Product Discount Amazing
+
+    //public class ProductDiscountAmazingViewComponent : ViewComponent
+    //{
+    //    private readonly IProductDiscountService _productDiscountService;
+
+    //    public ProductDiscountAmazingViewComponent(IProductDiscountService productDiscountService)
+    //    {
+    //        _productDiscountService = productDiscountService;
+    //    }
+
+    //    public async Task<IViewComponentResult> InvokeAsync()
+    //    {
+    //        var discountAmazing = await _productDiscountService.GetProductDiscountAmazing();
+    //        return View("ProductDiscountAmazing", discountAmazing);
+    //    }
+    //}
+    //#endregion
+}
+
